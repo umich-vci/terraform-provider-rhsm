3e0e5641a8c2b1899fdd7446f4f63f84a0e94294
Adam Robinson <adarobin@umich.edu>
2020-10-15 13:34:19 -0400

    ran `tf-sdk-migrator v2upgrade`

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.15
 
 require (
 	github.com/antihax/optional v1.0.0
-	github.com/hashicorp/terraform-plugin-sdk v1.7.0
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.3
 	github.com/umich-vci/gorhsm v1.143.0
-	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 )
--- rhsm/provider.go
+++ rhsm/provider.go
@@ -1,8 +1,8 @@
 package rhsm
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
