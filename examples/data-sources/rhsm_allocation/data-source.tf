2a3542d3644561ab489aadf327104da0ef7aeb76
Adam Robinson <adarobin@umich.edu>
2021-06-07 15:11:23 -0400

    add examples

---
+++ examples/data-sources/rhsm_allocation/data-source.tf
@@ -0,0 +1,3 @@
+resource "rhsm_allocation" "foo" {
+  uuid = "123e4567-e89b-12d3-a456-426655440000"
+}
---
+++ examples/data-sources/rhsm_allocation_entitlement/data-source.tf
@@ -0,0 +1,4 @@
+data "rhsm_allocation_entitlement" "entitlement" {
+  allocation_uuid = "123e4567-e89b-12d3-a456-426655440000"
+  entitlement_id  = "123e4567-e89b-12d3-a456-426655440000"
+}
