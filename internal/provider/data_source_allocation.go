fa6b523aa42b39136723d81776e7ae04654306a3
Adam Robinson <adarobin@umich.edu>
2021-06-10 17:33:27 -0400

    add missing attributes

--- internal/provider/data_source_allocation.go
+++ internal/provider/data_source_allocation.go
@@ -21,39 +21,14 @@ func dataSourceAllocation() *schema.Resource {
 				Required:     true,
 				ValidateFunc: validation.IsUUID,
 			},
-			"name": {
-				Description: "The name of the subscription allocation.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"type": {
-				Description: "The type of the subscription allocation.  The only one supported by this resource is `Satellite`.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"version": {
-				Description: "The version of the subscription allocation type.",
+			"created_by": {
+				Description: "The user account used to create the subscription allocation.",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
 			"created_date": {
 				Description: "The date and time the subscription allocation was created.",
 				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"created_by": {
-				Description: "The user account used to create the subscription allocation.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"last_modified": {
-				Description: "The date and time the subscription allocation was last modified.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"entitlements_attached_quantity": {
-				Description: "The number of entitlements associated with the subscription",
-				Type:        schema.TypeInt,
 				Computed:    true,
 			},
 			"entitlements_attached": {
@@ -72,8 +47,82 @@ func dataSourceAllocation() *schema.Resource {
 							Type:        schema.TypeBool,
 							Computed:    true,
 						},
+						"values": {
+							Description: "A list of entitlements attached to the subscription allocation.",
+							Type:        schema.TypeSet,
+							Computed:    true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"contract_number": {
+										Description: "The subscription contract number.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"end_date": {
+										Description: "The date the subscription ends.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"entitlement_quantity": {
+										Description: "The quantity of the subscription available in the entitlement.",
+										Type:        schema.TypeInt,
+										Computed:    true,
+									},
+									"id": {
+										Description: "The ID of the entitlement.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"sku": {
+										Description: "The SKU of the subscription.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"start_date": {
+										Description: "The date the subscription starts.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+									"subscription_name": {
+										Description: "The name of the subscription.",
+										Type:        schema.TypeString,
+										Computed:    true,
+									},
+								},
+							},
+						},
 					},
 				},
+			},
+			"entitlements_attached_quantity": {
+				Description: "The number of entitlements associated with the subscription",
+				Type:        schema.TypeInt,
+				Computed:    true,
+			},
+			"last_modified": {
+				Description: "The date and time the subscription allocation was last modified.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"name": {
+				Description: "The name of the subscription allocation.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"simple_content_access": {
+				Description: "Simple content access status.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"type": {
+				Description: "The type of the subscription allocation.  The only one supported by this resource is `Satellite`.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"version": {
+				Description: "The version of the subscription allocation type.",
+				Type:        schema.TypeString,
+				Computed:    true,
 			},
 		},
 	}
@@ -92,17 +141,33 @@ func dataSourceAllocationRead(ctx context.Context, d *schema.ResourceData, meta 
 	}
 
 	d.SetId(alloc.Body.GetUuid())
-	d.Set("name", alloc.Body.GetName())
-	d.Set("type", alloc.Body.GetType())
-	d.Set("version", alloc.Body.GetVersion())
-	d.Set("created_date", alloc.Body.GetCreatedDate())
 	d.Set("created_by", alloc.Body.GetCreatedBy())
+	d.Set("created_date", alloc.Body.GetCreatedDate())
+	d.Set("entitlements_attached_quantity", alloc.Body.GetEntitlementsAttachedQuantity())
 	d.Set("last_modified", alloc.Body.GetLastModified())
-	d.Set("entitlements_attached_quantity", alloc.Body.GetLastModified())
+	d.Set("name", alloc.Body.GetName())
+	d.Set("simple_content_access", alloc.Body.GetSimpleContentAccess())
+	d.Set("type", alloc.Body.GetType())
+	d.Set("uuid", alloc.Body.GetUuid())
+	d.Set("version", alloc.Body.GetVersion())
+
+	valueList := make([]map[string]interface{}, 0)
+	for _, x := range alloc.Body.EntitlementsAttached.GetValue() {
+		value := make(map[string]interface{})
+		value["contract_number"] = x.GetContractNumber()
+		value["end_date"] = x.GetEndDate()
+		value["entitlement_quantity"] = x.GetEntitlementQuantity()
+		value["id"] = x.GetId()
+		value["sku"] = x.GetSku()
+		value["start_date"] = x.GetStartDate()
+		value["subscription_name"] = x.GetSubscriptionName()
+		valueList = append(valueList, value)
+	}
 
 	entitlementsAttached := make(map[string]interface{})
 	entitlementsAttached["reason"] = alloc.Body.EntitlementsAttached.GetReason()
 	entitlementsAttached["valid"] = alloc.Body.EntitlementsAttached.GetValid()
+	entitlementsAttached["value"] = valueList
 	entitlementsAttachedList := []map[string]interface{}{entitlementsAttached}
 	d.Set("entitlements_attached", entitlementsAttachedList)
 
