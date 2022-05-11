0f8996115fdf2254f7db72c7d16925b8743e8f89
Adam Robinson <adarobin@umich.edu>
2022-05-11 11:51:32 -0400

    this test works with a Red Hat Developer Account

--- internal/provider/resource_cloud_access_account_test.go
+++ internal/provider/resource_cloud_access_account_test.go
@@ -26,10 +26,66 @@ func TestAccResourceCloudAccessAccountAzure(t *testing.T) {
 	})
 }
 
+func TestAccResourceCloudAccessAccountAWS(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceCloudAccessAccountAWS,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.aws_test_account", "nickname", "Terraform Acceptance Test AWS Account"),
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.aws_test_account", "account_id", "012345678912"),
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.aws_test_account", "provider_short_name", "AWS"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceCloudAccessAccountGCP(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceCloudAccessAccountGCP,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.gcp_test_account", "nickname", "Terraform Acceptance Test GCP Group"),
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.gcp_test_account", "account_id", "my.group@example.com"),
+					resource.TestCheckResourceAttr(
+						"rhsm_cloud_access_account.gcp_test_account", "provider_short_name", "GCE"),
+				),
+			},
+		},
+	})
+}
+
 const testAccResourceCloudAccessAccountAzure = `
 resource "rhsm_cloud_access_account" "azure_test_account" {
 	account_id          = "123e4567-e89b-12d3-a456-426614174000"
 	provider_short_name = "MSAZ"
 	nickname            = "Terraform Acceptance Test Azure Account"
+  }
+`
+
+const testAccResourceCloudAccessAccountAWS = `
+resource "rhsm_cloud_access_account" "aws_test_account" {
+	account_id          = "012345678912"
+	provider_short_name = "AWS"
+	nickname            = "Terraform Acceptance Test AWS Account"
+  }
+`
+
+const testAccResourceCloudAccessAccountGCP = `
+resource "rhsm_cloud_access_account" "gcp_test_account" {
+	account_id          = "my.group@example.com"
+	provider_short_name = "GCE"
+	nickname            = "Terraform Acceptance Test GCP Group"
   }
 `
