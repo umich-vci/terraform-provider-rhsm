bf55f18291ec4614e7be2e16d96dba62277fec88
Adam Robinson <adarobin@umich.edu>
2024-03-25 11:58:59 -0400

    add upgrade test

--- internal/provider/data_source_cloud_access_test.go
+++ internal/provider/data_source_cloud_access_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
 func TestAccDataSourceCloudAccess(t *testing.T) {
@@ -14,6 +15,32 @@ func TestAccDataSourceCloudAccess(t *testing.T) {
 			{
 				Config: testAccDataSourceCloudAccess,
 				Check:  resource.ComposeTestCheckFunc(),
+			},
+		},
+	})
+}
+
+func TestCloudAccessDataSource_UpgradeFromVersion(t *testing.T) {
+	/* ... */
+	resource.Test(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ExternalProviders: map[string]resource.ExternalProvider{
+					"rhsm": {
+						VersionConstraint: "0.6.1",
+						Source:            "umich-vci/rhsm",
+					},
+				},
+				Config: testAccDataSourceCloudAccess,
+			},
+			{
+				ProtoV6ProviderFactories: protoV6ProviderFactories(),
+				Config:                   testAccDataSourceCloudAccess,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectEmptyPlan(),
+					},
+				},
 			},
 		},
 	})
